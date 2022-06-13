e795509dcbba5733b3631cba4466c2a43c7c0ce4
Battlehart <sarah.stelljes@gmail.com>
2022-06-13 15:17:24 -0400

    added skeleton base to html file and started working on js.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var appId = "47a754a3120aa2e98794ad83a18b147e";
+var usIsoCode = 840;
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
